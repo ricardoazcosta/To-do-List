c501ed6c4ca3e19bd066b49b19e25c9326373897
ricardoazcosta <ricardozanete@gmail.com>
2022-08-10 18:04:46 -0300

    app-todo1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public todos: Todo[] = []; //----definido como Vazio//
+//  public todos: any[]; ---definido como Udefined--//
+
+ 
+public title: string='Minhas Tarefas'
+constructor() {
+  this.todos.push(new Todo(1, 'Meditação, Oração', false));
+  this.todos.push(new Todo(2,'Estudos', false));
+  this.todos.push(new Todo(3,'Levar Filhos na Escola',false));
+  this.todos.push(new Todo(4,'Estudos', false));
+    
   
+}
+
 }
---
+++ src/models/todo.model.ts
@@ -0,0 +1,13 @@
+export class Todo{
+      
+      constructor(
+        public id: Number,
+        public title: String,
+        public done: Boolean
+      ) {
+       
+      }
+   
+        
+    }
+    
