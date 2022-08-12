c8dfec1f8f1aa02ee907283b1ab731cedc11ff73
ricardoazcosta <ricardozanete@gmail.com>
2022-08-12 18:36:12 -0300

    REMOVE BUTTON

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,10 +13,21 @@ public title: string='Minhas Tarefas'
 constructor() {
   this.todos.push(new Todo(1, 'Oração', false));
   this.todos.push(new Todo(2,'Estudos', false));
-  this.todos.push(new Todo(3,'Compromisso Familiar',false));
-  this.todos.push(new Todo(4,'Estudos', false));
-    
-  
+  this.todos.push(new Todo(3,'Compromisso Familiar',true));
+ 
 }
+  remove(todo: Todo) {
+const index = this.todos.indexOf(todo);
+if(index !== -1){
+this.todos.splice(index,1);
+}
+  }
+
+  markAsDone() {
+  
+  }
+  markAsUndone() {
+  
+  }
 
 }
--- src/models/todo.model.ts
+++ src/models/todo.model.ts
@@ -3,10 +3,8 @@ export class Todo{
       constructor(
         public id: Number,
         public title: String,
-        public done: Boolean
-      ) {
-       
-      }
+        public done: Boolean,
+      ) { }
    
         
     }
