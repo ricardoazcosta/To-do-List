52a46b95b6e8e511fd64bc17633688f7ca48304d
ricardoazcosta <ricardozanete@gmail.com>
2022-08-12 17:24:38 -0300

    todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,11 @@ import { Todo } from 'src/models/todo.model';
 export class AppComponent {
   public todos: Todo[] = []; //----definido como Vazio//
 //  public todos: any[]; ---definido como Udefined--//
-
- 
 public title: string='Minhas Tarefas'
 constructor() {
-  this.todos.push(new Todo(1, 'Meditação, Oração', false));
+  this.todos.push(new Todo(1, 'Oração', false));
   this.todos.push(new Todo(2,'Estudos', false));
-  this.todos.push(new Todo(3,'Levar Filhos na Escola',false));
+  this.todos.push(new Todo(3,'Compromisso Familiar',false));
   this.todos.push(new Todo(4,'Estudos', false));
     
   
